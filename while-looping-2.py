a44a109a1172ce8889d2485b816705ba29733fec
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-03-06 21:31:52 +0700

    perulangan dengan WHILE dengan jumlah tak terbatas dan ada variabel pemberhenti

---
+++ while-looping-2.py
@@ -0,0 +1,25 @@
+"""
+Program perulangan menonton dan memahami makna Film dengan WHILE
+"""
+
+jumlahFilm = 10
+print("Kamu harus menonton semua Film dan pahami maknanya")
+
+ditonton = 0
+dipahami = 0
+print(f"Jumlah film yang telah ditonton dan dipahami {dipahami}")
+
+while ditonton < jumlahFilm * 2:
+    ditonton = ditonton + 1
+    if dipahami == 9:
+        print(f"Film ke {dipahami + 1} belum paham")
+    else:
+        dipahami = dipahami + 1
+        print(f"Film ke {dipahami} telah ditonton dan dipahami")
+
+print(f"Jumlah film yang telah ditonton dan dipahami {dipahami}")
+if dipahami == jumlahFilm:
+    print("Semua film telah ditonton dan dipahami")
+else:
+    print(f"Tidak semua film bisa dipahami. "
+          f"Saya hanya bisa memahami {dipahami} film")
