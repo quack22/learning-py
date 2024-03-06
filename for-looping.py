c271b03cf7ae4aefe8461ae17bbe04c02bcf25df
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-03-06 20:24:34 +0700

    looping menggunakan for

---
+++ for-looping.py
@@ -0,0 +1,15 @@
+"""
+Program perulangan menonton DVD
+"""
+
+jumlahDVD = 100
+print('Ayah berkata, "Tonton semua DVD-mu"')
+
+telahDitonton = 0
+print(f"jumlah DVD yang telah ditonton {telahDitonton} judul")
+
+print("Dengan for")
+for telahDitonton in range(1, jumlahDVD+1):
+    print(f"Buku ke {telahDitonton} sudah ditonton")
+
+print(f"Jumlah DVD yang sudah ditonton {telahDitonton} judul")
