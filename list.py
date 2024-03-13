759df83ca95f8eb45cba7ffe3c97ffbf82a9d8e0
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-03-13 16:47:44 +0700

    list comprehension

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+# Contoh penggunaan list di Python
 daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First']
 print(daftarBuku)
 
@@ -22,5 +23,60 @@ daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '
 print('Tambahkan satu buku baru')
 daftarBuku.append('Atomic Habits')
 
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nClear List')
+daftarBuku.clear()
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nGanti elemen pertama')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftarBuku[0] = 'Eight Habits'
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftarBuku.pop(1)
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop')
+daftarBuku.pop()
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+# Berguna ketika bermain dengan tipe data stack
+print('\nPop2')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+daftarBuku.pop(-1)
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nPerintah del')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftarBuku[0]
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nPerintah del dengan list comprehension')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftarBuku[:] # start:stop, contoh disamping berarti menghapus semua elemen
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nPerintah del dengan list comprehension start')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftarBuku[0:-1] # start:stop, contoh disamping berarti menghapus semua elemen
+for i in range(0, len(daftarBuku)):
+    print(daftarBuku[i])
+
+print('\nPerintah del dengan list comprehension step')
+daftarBuku = ['Seven Habits', 'How to Influence People', 'First Things First', '4DX']
+del daftarBuku[0::3] # start:stop:step, contoh disamping berarti menghapus elemen 0 dan 3.
 for i in range(0, len(daftarBuku)):
     print(daftarBuku[i])
